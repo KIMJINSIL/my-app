5f38d911196c729edb8fb1b0cbdb63eca591df86
kimjinsil <skyisrrna@naver.com>
2023-04-04 16:18:45 +0900

    블라블라

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
       <h1>안녕하세요!</h1>
       <h2>음.. 쉽지 않군요..</h2>
       <h3>공부를 많이 해야겠어요</h3>
+
+      <form>
+        <h3>입력해보아요</h3>
+        <div>
+          <label for="user_name">이름이 뭐에요?</label>  
+          <input type="text" id="user_name"></input>
+        </div>
+      </form>
     </div>
   );
 }
